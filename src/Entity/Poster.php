526f8b449562fea070a29065217390d9d04f5347
Albin Blachon <albin.blachon@etudiant.univ-reims.fr>
2025-06-10 17:04:22 +0200

    feats: add findById method

--- src/Entity/Poster.php
+++ src/Entity/Poster.php
@@ -1,9 +1,13 @@
 <?php
-// Albin Blachon  
+// Albin Blachon
 
 declare(strict_types=1);
 
 namespace Entity;
+
+use Database\MyPdo;
+use Exception\EntityNotFoundException;
+use PDO;
 
 class Poster
 {
@@ -26,4 +30,28 @@ class Poster
         return $this->id;
     }
 
+    /** Search the poster by his id.
+     *
+     * @param int $id The poster id.
+     * @return Poster The poster.
+     * @throws EntityNotFoundException If no Poster was found.
+     */
+    public static function findById(int $id): Poster
+    {
+
+        $stmt = MyPdo::getInstance()->prepare(
+            <<<SQL
+            SELECT *
+            FROM poster
+            WHERE id = :id
+            SQL
+        );
+        $stmt->bindValue(':id', $id);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, Poster::class);
+        $stmt-> execute();
+        if(($resp = $stmt->fetch()) === false){
+            throw new EntityNotFoundException();
+        }
+        return $resp;
+    }
 }
