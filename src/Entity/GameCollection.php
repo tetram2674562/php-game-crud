8fba0dd2b6c5a4efb4224ba6b7fbbe7e2bf30885
Albin Blachon <albin.blachon@etudiant.univ-reims.fr>
2025-06-10 16:39:17 +0200

    feat: add findGameByGenreId method

--- src/Entity/GameCollection.php
+++ src/Entity/GameCollection.php
@@ -1,5 +1,6 @@
 <?php
-// Albin Blachon  
+
+// Albin Blachon
 
 declare(strict_types=1);
 
@@ -19,7 +20,8 @@ class GameCollection
      */
     public static function findGameByCollectionId(int $categoryId): array
     {
-        $stmt = MyPdo::getInstance()->prepare(<<<SQL
+        $stmt = MyPdo::getInstance()->prepare(
+            <<<SQL
             SELECT * 
             FROM game g 
                 INNER JOIN game_category c ON (c.gameId = g.id)
@@ -33,7 +35,30 @@ class GameCollection
             throw new EntityNotFoundException();
         }
         return $resp;
-
     }
-}
+
+    /** Search every games of a genre.
+     *
+     * @param int $genreId Genre Id.
+     * @return Game[] The list of games
+     * @throws EntityNotFoundException If no game is found.
+     */
+    public static function findGameByGenreId(int $genreId): array
+    {
+        $stmt = MyPdo::getInstance()->prepare(
+            <<<SQL
+            SELECT * 
+            FROM game g 
+                INNER JOIN game_genre c ON (c.gameId = g.id)
+            WHERE c.genreId = :genreId
+            SQL
+        );
+        $stmt->bindValue(':genreId', $genreId);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, Game::class);
+        $stmt->execute();
+        if (($resp = $stmt->fetchAll()) === false) {
+            throw new EntityNotFoundException();
+        }
+        return $resp;
+    }
 }
