8fe49769c102e25654c3dce128a2e14e51b9445b
Albin Blachon <albin.blachon@etudiant.univ-reims.fr>
2025-06-10 11:03:16 +0200

    feat: add findAll

---
+++ src/Entity/Collection/CategoryCollection.php
@@ -0,0 +1,29 @@
+<?php
+// Albin Blachon  
+
+declare(strict_types=1);
+
+namespace Entity\Collection;
+
+use Database\MyPdo;
+use Entity\Category;
+use PDO;
+
+class CategoryCollection
+{
+    /** Get every category of the table game_category
+     *
+     * @return Category[] All categories
+     */
+    public static function findAll(): array
+    {
+        $request = MyPdo::getInstance()->prepare(<<<SQL
+            SELECT * 
+            FROM game_category
+            SQL
+        );
+        $request->execute();
+        $request->setFetchMode(PDO::FETCH_CLASS, Category::class);
+        return $request->fetchAll();
+    }
+}
