ca58013063865cab89578a9c1b4e17815600a4b0
Aymeric Gaboyard <aymeric.gaboyard@etudiant.univ-reims.fr>
2025-06-10 16:34:44 +0200

    Fix : Added "s" after the text in h2 html element and removed html class category, genre and subtitle and also reordered to put the genre before the category.

--- public/index.php
+++ public/index.php
@@ -9,18 +9,20 @@ use Html\AppWebPage;
 // creation of the webpage
 $appWebPage = new AppWebPage("Jeux vidéos");
 // Creation of categories
+
+// Creation of genres
+$appWebPage->appendContent("<div class='genres'>\n");
+// creation of the subtitle for genres
+$appWebPage->appendContent("<h2 >Genres</h2>\n");
+foreach (GenreCollection::findAll() as $genre) {
+    $appWebPage->appendContent("<a href='genre.php?genreId={$genre->getId()}'>{$genre->getDescription()}</a>\n");
+}
+$appWebPage->appendContent("</div>");
 $appWebPage->appendContent("<div class='categories'>\n");
 // creation of the subtitle for category
-$appWebPage->appendContent("<h2 class='subtitle'>Catégorie</h2>\n");
+$appWebPage->appendContent("<h2>Catégories</h2>\n");
 foreach (CategoryCollection::findAll() as $category) {
-    $appWebPage->appendContent("<a href='categorie.php?categoryId={$category->getId()}' class='category'>{$category->getDescription()}</a>\n");
-}
-// Creation of genres
-$appWebPage->appendContent("</div>\n\t\t\t<div class='genres'>\n");
-// creation of the subtitle for genre
-$appWebPage->appendContent("<h2 class='subtitle'>Genre</h2>\n");
-foreach (GenreCollection::findAll() as $genre) {
-    $appWebPage->appendContent("<a href='genre.php?genreId={$genre->getId()}' class='genre'>{$genre->getDescription()}</a>\n");
+    $appWebPage->appendContent("<a href='categorie.php?categoryId={$category->getId()}'>{$category->getDescription()}</a>\n");
 }
 // Closing every the box
 $appWebPage->appendContent("</div>");
