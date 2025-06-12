209cfde3b1532067a3ef71edc031ad65a7cbbd99
Aymeric Gaboyard <aymeric.gaboyard@etudiant.univ-reims.fr>
2025-06-12 09:04:23 +0200

    Feat : Added HTML category display page and displayed every game with link to their page.

--- public/category.php
+++ public/category.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use Entity\Category;
+use Entity\Collection\GameCollection;
 use Exception\EntityNotFoundException;
 use Exception\ParameterException;
 use Html\AppWebPage;
@@ -10,9 +12,27 @@ try {
     if (!isset($_GET["categoryId"]) || empty($_GET["categoryId"])) {
         throw new ParameterException();
     }
-
-    $appWebPage = new AppWebPage("Jeux Vidéo : {}");
-
+    // Find the category
+    $category = Category::findById(intval($_GET["categoryId"]));
+    // Create the new page
+    $appWebPage = new AppWebPage("Jeux Vidéo : {$category->getDescription()}");
+    // Get all the game for this category
+    $games = GameCollection::findGameByCategoryId($category->getId());
+    // For each games
+    foreach ($games as $game) {
+        $appWebPage->appendContent(
+            <<<HTML
+        <a href="game.php?gameId={$game->getId()}">
+            <img src="poster.php?posterId={$game->getPosterId()}" alt="{$game->getName()}">
+            <div>
+                <p>{$game->getName()} ({$game->getReleaseYear()})</p>
+                <p>{$game->getShortDescription()}</p>
+            </div>
+        </a>
+        HTML
+        );
+    }
+    echo $appWebPage->toHTML();
 } catch (EntityNotFoundException) {
     http_response_code(404);
 } catch (ParameterException) {
