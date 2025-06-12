72c4b1f6a2c1863c6454f9dd056272f65303a13e
Aymeric Gaboyard <aymeric.gaboyard@etudiant.univ-reims.fr>
2025-06-12 14:49:56 +0200

    Feat : Created barebones GameForm class

---
+++ src/Html/Form/GameForm.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Html\Form;
+
+use Entity\Game;
+use Html\StringEscaper;
+
+class GameForm
+{
+    use StringEscaper;
+
+    private ?Game $game;
+
+    /** Create a new form
+     *
+     * @param Game|null $game A game (or null if there is no game).
+     */
+    public function __construct(?Game $game = null)
+    {
+        $this->game = $game;
+    }
+
+    /** Get the game of this form
+     *
+     * @return Game The game
+     */
+    public function getGame(): Game
+    {
+        return $this->game;
+    }
+
+}
