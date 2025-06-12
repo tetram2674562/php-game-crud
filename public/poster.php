c8f6dbb375bf06a28e27f423e57c1f8350d0d874
Albin Blachon <albin.blachon@etudiant.univ-reims.fr>
2025-06-12 09:22:04 +0200

    feat: add the poster.php file

---
+++ public/poster.php
@@ -0,0 +1,30 @@
+<?php
+
+// Albin BLACHON
+
+declare(strict_types=1);
+
+use Entity\Poster;
+use Exception\EntityNotFoundException;
+use Exception\ParameterException;
+
+try {
+    // if posterId is an int
+    if (isset($_GET['posterId']) && ctype_digit($_GET['posterId'])) {
+        // get the poster
+        $posterId = intval($_GET['posterId']);
+        $poster = Poster::findById($posterId);
+        // return the poster image
+        header('Content-Type: image/jpeg');
+        echo $poster->getJpeg();
+        // if posterId not define or not an int throw an exception
+    } else {
+        throw new ParameterException('');
+    }
+} catch (EntityNotFoundException) {
+    http_response_code(404);
+} catch (ParameterException) {
+    http_response_code(400);
+} catch (Exception) {
+    http_response_code(500);
+}
