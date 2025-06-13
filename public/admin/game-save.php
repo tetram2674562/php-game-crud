61b7a7feddf1659de032a7abcfe2570ab1e71481
Aymeric Gaboyard <aymeric.gaboyard@etudiant.univ-reims.fr>
2025-06-13 16:20:26 +0200

    Feat : Added save file for games.

---
+++ public/admin/game-save.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Exception\EntityNotFoundException;
+use Exception\ParameterException;
+use Html\Form\GameForm;
+
+try {
+    $form = new GameForm();
+    $form->saveGameFromQueryString();
+    header("Location: /");
+} catch (EntityNotFoundException) {
+    http_response_code(404);
+} catch (ParameterException) {
+    http_response_code(400);
+}
+header("Location: /");
