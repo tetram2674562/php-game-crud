0c134c0bc36b388f51f6da1d77aca2a867d56274
Aymeric Gaboyard <aymeric.gaboyard@etudiant.univ-reims.fr>
2025-06-13 14:28:32 +0200

    Feat : Added support for editing games.

--- public/admin/game-form.php
+++ public/admin/game-form.php
@@ -2,12 +2,18 @@
 
 declare(strict_types=1);
 
+use Entity\Game;
 use Exception\EntityNotFoundException;
 use Exception\ParameterException;
 use Html\Form\GameForm;
 
 try {
-    $gameForm = new GameForm();
+    $game = null;
+    if (isset($_GET["gameId"]) && ctype_digit($_GET["gameId"])) {
+        $game = Game::findById(intval($_GET["gameId"]));
+
+    }
+    $gameForm = new GameForm($game);
     echo $gameForm->toHTML("game-save.php");
 } catch (ParameterException) {
     http_response_code(400);
