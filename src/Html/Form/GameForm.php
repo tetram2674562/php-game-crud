7673b68a7cedce1e0adbea13a0a2433bf80bae58
Aymeric Gaboyard <aymeric.gaboyard@etudiant.univ-reims.fr>
2025-06-13 09:23:09 +0200

    Feat : Added developer, category and genres selection in the form

--- src/Html/Form/GameForm.php
+++ src/Html/Form/GameForm.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Html\Form;
 
+use Entity\Collection\CategoryCollection;
+use Entity\Collection\DeveloperCollection;
+use Entity\Collection\GenreCollection;
 use Entity\Developer;
 use Entity\Game;
 use Exception\EntityNotFoundException;
@@ -44,6 +47,7 @@ class GameForm
 
         }
 
+
         $form = <<< HTML
         <!doctype html>
         <html lang="fr">
@@ -63,7 +67,7 @@ class GameForm
                     Nom <input type="text" name="name" id="name" value="{$this->escapeString($this?->getGame()?->getName())}" required> <br>
                 </label>
                 <label for="releaseYear">
-                    Date de sortie <input type="text" name="releaseYear" id="releaseYear" value="{$this->escapeString($this?->getGame()?->getReleaseYear())}" required><br>
+                    Date de sortie <input type="text" name="releaseYear" id="releaseYear" value="{$this->escapeString($this->getGame()?->getReleaseYear())}" required><br>
                 </label>
                 <label for="shortDescription">
                     Description courte <input type="text" name="shortDescription" id="shortDescription" value="{$this->escapeString($this?->getGame()?->getShortDescription())}" required><br>
@@ -83,20 +87,42 @@ class GameForm
                 <label for="metacritic">
                     Score metacritic <input type="number" name="metacritic" id="metacritic" value="{$this->escapeString($this->getGame()?->getMetacritic())}" required><br>
                 </label>
-                <!-- Get the developer from the database and make choice in list-->
-                <label for="developer">
-                    Développeur <input type="text" name="developer" id="developer" value="{$this->escapeString($developer?->getName())}"><br>
-                </label>
                 <!-- Add a new poster so It's a file input -->
                 <label for="poster">
-                    Poster <input type="file" name="poster" id="poster" value=""><br>
+                    Poster <input type="file" name="poster" id="poster" value="" accept="image/jpeg"><br>
                 </label>
+        HTML;
+        $selected = $developer == null ? "selected='selected'" : "";
+        $form .= "\t\t\tDeveloper <select name='developer'>\n\t\t\t\t<option value='' {$selected}></option>";
+        foreach (DeveloperCollection::findAll() as $developers) {
+            $selected = $developer?->getName() == $developers->getName() ? "selected='selected'" : "";
+            $form .= "\t\t\t\t<option value='{$this->escapeString($developers->getName())}' {$selected}>{$this->escapeString($developers->getName())}</option>\n";
+        }
+
+        $form .= "\t\t\t</select>\n\t\t\t<div class='categories_select'>Categories";
+        foreach (CategoryCollection::findAll() as $category) {
+            $form.= <<< HTML
+                    <label>
+                        <input type="checkbox" name="categories[]" value="{$this->escapeString($category->getDescription())}"> {$this->escapeString($category->getDescription())}
+                    </label>
+            HTML;
+        }
+        $form .= "\t\t\t</div>\n\t\t\t<div class='genres_select'>Genres";
+        foreach (GenreCollection::findAll() as $genre) {
+            $form.= <<< HTML
+                    <label>
+                        <input type="checkbox" name="genres[]" value="{$this->escapeString($genre->getDescription())}"> {$this->escapeString($genre->getDescription())}
+                    </label>
+            HTML;
+        }
+        $form .= "\t\t\t</div>";
+        $form .= <<<HTML
                 <input type="submit" value="Enregistrer">
             </form>  
         </body>
         </html>
-              
         HTML;
+
         return $form;
     }
 }
